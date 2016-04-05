fb5e951c30f907e1596cf1c59f6c0a35e1d67fa0
sakilu <sakilu@gmail.com>
2016-04-05 13:06:52 +0800

    add crud, gitigore

--- install.sh
+++ install.sh
@@ -5,3 +5,4 @@ cp -r ./* ../;cd ..;rm -rf base
 cd application;
 composer update
 cd ..;git init;git add .;git commit -m "first commit";git remote add origin https://sakilu@bitbucket.org/sakilu/$project.git;git push -u origin master
+git clone https://github.com/sakilu/crud.git
