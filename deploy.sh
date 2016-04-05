ee8f991db646d20cbe547e02dc85ac0b956fdd65
sakilu <sakilu@gmail.com>
2016-04-05 13:28:20 +0800

    script bug

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,3 @@ cp -r ./* ../;cd ..;rm -rf base
 cd application;
 composer update
 cd ..;git clone https://github.com/sakilu/crud.git
-sudo /home/ubuntu/docker/virtualhost.sh create
-sudo /home/ubuntu/docker/virtualhost.sh delete
-sudo /home/ubuntu/docker/virtualhost.sh create
