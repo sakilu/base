7fc108d16cf2afdcf27d54f8b18d31fb20f01cc5
sakilu <sakilu@gmail.com>
2016-04-05 12:55:05 +0800

    script bug 修正

--- install.sh
+++ install.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
  
 project="$1"
-git clone https://github.com/sakilu/base.git
-cd base;cp -r ./* ../;cd ..;rm -rf base
+cp -r ./* ../;cd ..;rm -rf base
 cd application;
 composer update
 cd ..;git init;git add .;git commit -m "first commit";git remote add origin https://sakilu@bitbucket.org/sakilu/$project.git;git push -u origin master
