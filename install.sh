ceb13e5ab7fa5abfdc393949f4ecc6fee6c1027e
sakilu <sakilu@gmail.com>
2016-04-06 10:40:18 +0800

    update script

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
  
 project="$1"
-cp -r ./* ../;cd ..;rm -rf base
+cp -r ./ ../;cd ..;rm -rf base
 cd application;
 composer update
 cd ..;git init;git add .;git commit -m "first commit";git remote add origin https://sakilu@bitbucket.org/sakilu/$project.git;git push -u origin master
