14d47b44cefd26451e3bff5a598a023079828175
sakilu <sakilu@gmail.com>
2016-04-09 22:57:47 +0800

    script bug

--- install.sh
+++ install.sh
@@ -4,5 +4,5 @@ project="$1"
 cp -r ./ ../;cd ..;rm -rf base
 cd application;
 composer update
-cd ..;git init;git add .;git commit -m "first commit";
+cd ..;rm -rf .git;git init;git add .;git commit -m "first commit";
 git clone https://github.com/sakilu/crud.git
